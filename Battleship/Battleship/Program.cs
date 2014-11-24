7b1a750f195414cca2483c0cdbeea7f04251cac0
Eric Fowler <ericfowler303@gmail.com>
2014-11-24 16:24:05 -0700

    Testing displaying the grid with objects

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -10,17 +10,96 @@ namespace Battleship
     {
         static void Main(string[] args)
         {
-            BattleShipGame game = new BattleShipGame();
-            
+            BattleShipGame game = new BattleShipGame(10,10);
+
+            Console.ReadKey();
         }
 
     }
 
     class BattleShipGame
     {
-        public BattleShipGame()
+        private int GridX { get; set; }
+        private int GridY { get; set; }
+        List<Ship> listOfShips = new List<Ship>();
+        List<Point> UserMoves = new List<Point>();
+        public bool AllShipsDestroyed
         {
+            get{return listOfShips.All(x => x.isDestroyed == true);}
+        }
+        public int CombatRound { get; set; }
+        public BattleShipGame(int xLength, int yLength)
+        {
+            this.GridX = xLength;
+            this.GridY = yLength;
             // Init ships
+            InitShips();
+
+            DrawGrid();
+
+        }
+        private void DrawGrid()
+        {
+            // Loop through every coordinate in the grid
+            for (int y = 0; y < GridY; y++)
+            {
+                for (int x = 0; x < GridX; x++)
+                {
+                    // Figure out if any ship or user action is in this coordinate
+                    if (IsShipHere(x,y))
+                    {
+                        // Is a ship here
+                        Console.Write("[x]");
+
+                    } else {
+                        // Nothing here
+                        Console.Write("[ ]");
+                    }
+                }
+                Console.Write("\n");
+            }
+            // Print status messages here
+        }
+
+        private bool IsShipHere(int x, int y)
+        {
+            return listOfShips.SelectMany(a => a.occupiedPoints).Any(b => b.xVal == x && b.yVal == y);
+        }
+
+        private void InitShips()
+        {
+            Ship ship1 = new Ship(Ship.ShipType.Carrier);
+            ship1.occupiedPoints.Add(new Point(0,0));
+            ship1.occupiedPoints.Add(new Point(0, 1));
+            ship1.occupiedPoints.Add(new Point(0, 2));
+            ship1.occupiedPoints.Add(new Point(0, 3));
+            ship1.occupiedPoints.Add(new Point(0, 4));
+
+            Ship ship2 = new Ship(Ship.ShipType.Battleship);
+            ship2.occupiedPoints.Add(new Point(2, 0));
+            ship2.occupiedPoints.Add(new Point(3, 0));
+            ship2.occupiedPoints.Add(new Point(4, 0));
+            ship2.occupiedPoints.Add(new Point(5, 0));
+
+            Ship ship3 = new Ship(Ship.ShipType.Submarine);
+            ship3.occupiedPoints.Add(new Point(1, 8));
+            ship3.occupiedPoints.Add(new Point(2, 8));
+            ship3.occupiedPoints.Add(new Point(3, 8));
+
+            Ship ship4 = new Ship(Ship.ShipType.Cruiser);
+            ship4.occupiedPoints.Add(new Point(4, 6));
+            ship4.occupiedPoints.Add(new Point(4, 7));
+            ship4.occupiedPoints.Add(new Point(4, 8));
+
+            Ship ship5 = new Ship(Ship.ShipType.Minesweeper);
+            ship5.occupiedPoints.Add(new Point(7, 4));
+            ship5.occupiedPoints.Add(new Point(7, 5));
+
+            listOfShips.Add(ship1);
+            listOfShips.Add(ship2);
+            listOfShips.Add(ship3);
+            listOfShips.Add(ship4);
+            listOfShips.Add(ship5);
         }
     }
 
